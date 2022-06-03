073cc7a684858bdcc596287ec21936c39c2c1341
Rafael Z Tanganelli <kabelu@gmail.com>
2022-06-03 21:27:01 +0200

    Using arguments to write a uppercased message

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
 """
-Print 'hello world!'
+Print an uppercased arg
+
+How to use:
+$ ./hello.py <arg>
+
 
 """
+__version__ = "0.1.0"
+__author__ = "Rafael Z Tanganelli"
 
-msg = "hello, world!"
+import sys
+
+msg = sys.argv[1]
 
 print(f"{msg.upper()}")
 
