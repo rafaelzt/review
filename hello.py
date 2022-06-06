cb8fb334030050bb74355dfdbdc2715c651341c4
Rafael Z Tanganelli <kabelu@gmail.com>
2022-06-06 10:04:28 +0200

    Reading env_vars

--- hello.py
+++ hello.py
@@ -10,12 +10,28 @@ $ ./hello.py <arg>
 """
 __version__ = "0.1.0"
 __author__ = "Rafael Z Tanganelli"
+__license__ = "Unlicense"
 
 import sys
+import os
 
 arguments = sys.argv[1:]
+language = os.getenv("LANG", "en_US.UTF-8")
 
-for arg in arguments:
-    print(f"{arg.upper()}")
+language = language.split(".")[0]
 
+
+msg = {
+    "en_US": "Hello, world!",
+    "pt_BR": "Olá, mundo!",
+    "fr_FR":"Bon jour, monde",
+    "it_IT":"Ciao, mondo!",
+}
+
+
+if arguments:
+    for arg in arguments:
+        print(f"{arg.upper()}")
+else:
+    print(f"{msg[language]}")
 
